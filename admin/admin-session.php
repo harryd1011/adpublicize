3b6eff1afe5c9d156e8c3ae2f8ce8aee452462b4
harryd1011 <20ce028@charusat.edu.in>
2022-10-29 12:27:37 +0530

    Redesigning of admin dashboard.

--- admin/admin-session.php
+++ admin/admin-session.php
@@ -6,10 +6,21 @@ $admin= $_SESSION['logadmin'];
 $fetch_admin = mysqli_query($conn,"SELECT * FROM admin_1 where username= '$admin'");
 
 $row_admin = mysqli_fetch_object($fetch_admin);
+$num_rows = mysqli_num_rows($fetch_admin);
 
 $admin_username= $row_admin->username;
 $admin_password= $row_admin->password;
 
+// To fetch the number of bookings done
+$fetch_bookings = mysqli_query($conn,"SELECT * FROM booking_details");
+$num_bookings = mysqli_num_rows($fetch_bookings);
 
+// To fetch the number of slots left for booking
+$fetch_available = mysqli_query($conn,"SELECT * FROM slot_details_1");
+$num_available = mysqli_num_rows($fetch_available);
+$slot_available = $num_available - $num_bookings;
 
+// To fetch the number of users registered into website
+$fetch_users = mysqli_query($conn,"SELECT * FROM registration");
+$num_users = mysqli_num_rows($fetch_users);
 ?>
